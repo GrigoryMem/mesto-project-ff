f3e150e1a8a8624b784cb07bfcbdcea5c4b43be3
Grigory Morozov <morozehc@yandex.ru>
2024-09-25 23:31:01 +0300

    feat:add func createCard

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,21 @@ const cardTemplate = document.querySelector('#card-template').content; // соз
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+function createCard(data){
+  // клонировать шаблон кароточки - создал экземпляр карточки
+  const cardTemplateClone = cardTemplate.cloneNode(true); 
+  const cardExample =  cardTemplateClone.querySelector('.card');
+  //установить значения вложенных элементов
+  cardExample .querySelector('.card__image').src = data.link;
+  cardExample .querySelector('.card__title').textContent = data.name;
+  // удаление карточки
+  removeCard(cardExample);
+  // поставить лайк
+  likeCard(cardExample);
+  
+  return cardExample;
+}
+
 
 // @todo: Функция удаления карточки
 
