57086de0efca04394c6c3757300bdca389edd381
Grigory Morozov <morozehc@yandex.ru>
2024-09-26 15:39:24 +0300

    refactor: add function render with circle cards

--- scripts/index.js
+++ scripts/index.js
@@ -10,17 +10,17 @@ const placesList = document.querySelector('.places__list');
 
 function createCard(data) {
   // клонировать шаблон кароточки - создал экземпляр карточки
-  const cardTemplateClone = cardTemplate.cloneNode(true); 
-  const cardExample =  cardTemplateClone.querySelector('.card');
+  const cardTemplateClone = cardTemplate.cloneNode(true);
+  const cardExample = cardTemplateClone.querySelector('.card');
   //установить значения вложенных элементов
-  cardExample .querySelector('.card__image').src = data.link;
-  cardExample .querySelector('.card__title').textContent = data.name;
+  cardExample.querySelector('.card__image').src = data.link;
+  cardExample.querySelector('.card__title').textContent = data.name;
   cardExample.querySelector('.card__image').alt = data.name;
   // удаление карточки
   removeCard(cardExample);
   // поставить лайк
   likeCard(cardExample);
-  
+
   return cardExample;
 }
 
@@ -28,20 +28,27 @@ function createCard(data) {
 
 function removeCard(card) {
   const cardDelBtn = card.querySelector('.card__delete-button');
-  cardDelBtn.addEventListener('click',()=>{
+  cardDelBtn.addEventListener('click', () => {
     cardDelBtn.closest('.card').remove();
-  })
+  });
 }
 
 // @todo: Функция лайка карточки
 
 function likeCard(card) {
   const cardLkBtn = card.querySelector('.card__like-button');
-  cardLkBtn.addEventListener('click',()=>{
-  cardLkBtn.classList.toggle('card__like-button_is-active');
-  })
+  cardLkBtn.addEventListener('click', () => {
+    cardLkBtn.classList.toggle('card__like-button_is-active');
+  });
 }
 
 // @todo: Вывести карточки на страницу
 
-initialCards.forEach(item => placesList.append(createCard(item)));
+// initialCards.forEach((item) => placesList.append(createCard(item)));
+function renderCards() {
+  for(let i = 0; i<initialCards.length;i++) {
+    placesList.append(createCard(initialCards[i]));
+  }
+}
+  
+renderCards();
