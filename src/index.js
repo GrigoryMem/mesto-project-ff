a59b78cdfe14facc8bde4280e3016884271fa245
Grigory Morozov <morozehc@yandex.ru>
2024-10-20 14:54:58 +0300

    ref: divide modules js: create renderCards.js

--- src/index.js
+++ src/index.js
@@ -1,60 +1,5 @@
-// @todo: Темплейт карточки
 import './pages/index.css'; // добавьте импорт главного файла стилей 
-import {initialCards} from "./scripts/cards.js";
-
-
-const cardTemplate = document.querySelector('#card-template').content; // создал шаблон карточки
-
-// @todo: DOM узлы
-
-const placesList = document.querySelector('.places__list');
-
-// @todo: Функция создания карточки
-
-function createCard(data, removeCard, likeCard) {
-  // клонировать шаблон кароточки - создал экземпляр карточки
-  const cardTemplateClone = cardTemplate.cloneNode(true);
-  const cardExample = cardTemplateClone.querySelector('.card');
-  const cardExmpImg = cardExample.querySelector('.card__image');
-  const cardLikeBtn = cardExample.querySelector('.card__like-button');
-  //установить значения вложенных элементов
-  cardExmpImg.src = data.link;
-  cardExample.querySelector('.card__title').textContent = data.name;
-  cardExmpImg.alt = data.name;
-
-  // событие - удаление карточки
-  cardExample.querySelector('.card__delete-button').addEventListener('click',()=> {
-    removeCard(cardExample);
-  })
-
-  // событие - поставить лайк
-  cardLikeBtn.addEventListener('click',()=> {
-    likeCard(cardLikeBtn);
-  })
-  return cardExample;
-}
-
-// @todo: Функция удаления карточки
-
-function removeCard(card) {
-  card.remove();
-}
-
-// @todo: Функция лайка карточки
-
-function likeCard(card) {
-  card.classList.toggle('card__like-button_is-active');
-}
-
-// @todo: Вывести карточки на страницу
-
-function renderCards() {
-  initialCards.forEach((item) => placesList.append(createCard(item,removeCard,likeCard)));
-}
-  
-renderCards(); // отобразить карточки на странице
-
-
+import './scripts/renderCards' // запуск карточек
 
 
 // 3. Работа модальных окон
@@ -90,7 +35,7 @@ btnEditPrfl.addEventListener('click',()=>{
 })
 // собыитие закрытия окна при нажатии на крестик
 popupEdit.addEventListener('click',(event)=>{
-  closeModal(event,popupEdit)
+  closeModal(event,popupEdit);
 })
 
 
