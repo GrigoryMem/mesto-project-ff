f8a6840a01c2f611b836d8f4ccab4eec7bae7897
Grigory Morozov <morozehc@yandex.ru>
2024-10-25 21:23:23 +0300

    ref:delete comments

--- src/scripts/components/card.js
+++ src/scripts/components/card.js
@@ -13,7 +13,6 @@ export function createCard(setCard,elem) {
   cardExample.querySelector('.card__delete-button').addEventListener('click',()=> {
     removeCard(cardExample);
   })
-
   // событие - поставить лайк
   cardLikeBtn.addEventListener('click',()=> {
     setCard.acts.like(cardLikeBtn) // лайк карточки
@@ -37,31 +36,4 @@ export function likeCard(card) {
   card.classList.toggle('card__like-button_is-active');
 }
 
-
-// export function createCard(data, removeCard, likeCard, template, openCard,openModal) {
-  //   // клонировать шаблон кароточки - создал экземпляр карточки
-  //   const cardTemplateClone =  template.cloneNode(true);
-  //   const cardExample = cardTemplateClone.querySelector('.card');
-  //   const cardExmpImg = cardExample.querySelector('.card__image');
-  //   const cardLikeBtn = cardExample.querySelector('.card__like-button');
-  //   const image = cardExample.querySelector('.card__image');
-  //   cardExmpImg.src = data.link;
-  //   cardExample.querySelector('.card__title').textContent = data.name;
-  //   cardExmpImg.alt = data.name;
-  //   // событие - удаление карточки
-  //   cardExample.querySelector('.card__delete-button').addEventListener('click',()=> {
-  //     removeCard(cardExample);
-  //   })
-  
-  //   // событие - поставить лайк
-  //   cardLikeBtn.addEventListener('click',()=> {
-  //     likeCard(cardLikeBtn);
-  //   })
-  //   // cобытие открыть картинку
-  //   image.addEventListener('click',()=>{
-  //     openCard(cardExample,image,openModal)
-  //   })
-    
-  //   return cardExample;
-  // }
   
