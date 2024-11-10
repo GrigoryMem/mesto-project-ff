3c47ec726cdb3aaedfd5baf27d4777af533812bf
Grigory Morozov <morozehc@yandex.ru>
2024-11-10 19:17:05 +0300

    feat: create file for validation forms

---
+++ src/scripts/components/validation.js
@@ -0,0 +1,24 @@
+// включение валидации вызовом enableValidation
+// все настройки передаются при вызове
+
+const popupValid = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+}
+
+
+
+const enableValidation = (elemsValid)=>{
+// включение валидации всех форм
+
+
+};
+
+const clearValidation = (profileForm, validationConfig)=>{
+  // очищает ошибки валидации формы и делает кнопку неактивной
+  
+}
