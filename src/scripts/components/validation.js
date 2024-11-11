436bb731e23aeebde252eb4328e9ef2301e29637
Grigory Morozov <morozehc@yandex.ru>
2024-11-11 14:05:36 +0300

    feat: add comments for funcs errors

--- src/scripts/components/validation.js
+++ src/scripts/components/validation.js
@@ -2,7 +2,7 @@ const forms = document.forms;
 const formProf = forms['edit-profile'];
 const formProfInput = formProf.querySelector('.popup__input_type_name');
 
-const formError = formProf.querySelector(`.${formProfInput.id}-error`);
+
 
 
 
@@ -40,33 +40,42 @@ const clearValidation = (profileForm, validationConfig)=>{
 
 
 // Функция, которая добавляет класс с ошибкой
-const showInputError = (elem,message,textErr)=>{
-    elem.classList.add('popup__input_type_error');
-    console.log(elem.validity)
-    message.classList.add('popup__error_visible');
-    message.textContent = textErr;
+const showInputError = (form,input,textErr)=>{
+  // Находим элемент с ошибкой через форму по id input
+  const errorElem = form.querySelector(`.${input.id}-error`);
+  // добавляем стили ошибки для поля ввода
+  input.classList.add('popup__input_type_error');
+  // показываем элемент с ошибкой
+  errorElem .classList.add('popup__error_visible');
+  // добавляем тип ошибки в поле для ошибки
+  errorElem .textContent = textErr;
 }
 // Функция, которая удаляет класс с ошибкой
-const hideInputError = (elem,message)=>{
-  elem.classList.remove('popup__input_type_error');
-  message.classList.remove('popup__error_visible');
-  message.textContent = '';
+const hideInputError = (form,input)=>{
+  // Находим элемент с ошибкой через форму по id input
+  const errorElem = form.querySelector(`.${input.id}-error`);
+  // убираем стили ошибки для поля ввода
+  input.classList.remove('popup__input_type_error');
+  // скрываем элемент с ошибкой
+  errorElem .classList.remove('popup__error_visible');
+  // очищаем поле текста ошибки
+  errorElem .textContent = '';
 }
 
 
 // Функция, которая проверяет валидность поля
-const isValid = (input,error)=>{
+const isValid = (form,input)=>{
   if(!input.validity.valid){
-    showInputError(input,error,input.validationMessage)
+    showInputError(form,input,input.validationMessage)
   }else{
-    hideInputError(input,error)
+    hideInputError(form,input)
   }
 }
 
 // Вызовем функцию isValid на каждый ввод символа
 
 formProf.addEventListener('input', ()=>{
-  isValid(formProfInput,formError);
+  isValid(formProf,formProfInput);
 });
 
 
